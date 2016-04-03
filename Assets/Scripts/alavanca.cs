dd05d62ad39dd770db6ec4a30d3020750aa1e5cc
paulodgn <paulo.nwork@gmail.com>
2016-04-03 12:59:41 +0100

    alavanca com sistema de particulas

---
+++ Assets/Scripts/alavanca.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class alavanca : MonoBehaviour {
+
+    Animation anim;
+    public ParticleSystem particle;
+	// Use this for initialization
+	void Start () 
+    {
+        anim = GetComponent<Animation>();
+        //particle = GetComponentInParent<ParticleSystem>();
+        anim.Stop();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    
+	}
+
+    void OnTriggerEnter()
+    {
+        anim.Play();
+        particle.Play();
+    }
+}
