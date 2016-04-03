b2a2a4ae025d77b9639d147db1482f4b9fb3ced7
paulodgn <paulo.nwork@gmail.com>
2016-04-03 01:46:41 +0100

    IA

---
+++ Assets/Scripts/IA/EnemyPatrolAndFollow.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPatrolAndFollow : MonoBehaviour {
+
+    GameObject player;
+    //PlayerHealth playerHealth;
+    public Transform[] patrolPoints;
+    int patrolPointID;
+    NavMeshAgent enemyAgent;
+    //DroneAlarm droneAlarm;
+    Transform PlayerPosition;
+    Animator anim;
+    //EnemyHealth enemyHealth;
+    //public AudioSource die;
+    //public AudioSource shoot;
+    public bool alarm;
+	void Start () 
+    {
+        patrolPointID = 0;
+        player = GameObject.FindGameObjectWithTag("Player");
+        //playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+        enemyAgent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        nextPatrolSpot();
+        //droneAlarm = GameObject.FindGameObjectWithTag("Drone").GetComponent<DroneAlarm>();
+        anim.SetBool("Walk", true);
+        //enemyHealth = gameObject.GetComponent<EnemyHealth>();
+        alarm = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //se inimigo esta morto desativa navAgent
+     
+        //se inimigo esta vivo procura proximo alvo
+        if (enemyAgent.enabled == true )
+        {
+            //se alarme esta desligado vai para o proximo ponto de patrulha
+            if (enemyAgent.remainingDistance < 0.5f /*&& !droneAlarm.alarm*/)
+            {
+                nextPatrolSpot();
+            }
+            //se alarme esta ligado vai de encontro ao player
+            if (alarm)
+            {
+                goToPlayer();
+            }
+        }
+
+	}
+
+    void nextPatrolSpot()
+    {
+        if (patrolPointID < patrolPoints.Length)
+        {
+            patrolPointID++;
+        }
+        else
+        {
+            patrolPointID = 0;
+        }
+
+        enemyAgent.destination = patrolPoints[patrolPointID].position;
+    }
+    //vai se encontro ao jogador
+    void goToPlayer()
+    {
+        //if (playerHealth.playerHealth > 0)
+       // {
+            enemyAgent.destination = player.transform.position;
+        //}
+       // else
+        //{
+        //    droneAlarm.alarm = false;
+        //    patrolPointID = 0;
+        //    nextPatrolSpot();
+        //}
+    }
+    //se encontrar o jogador
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject==player /*&& playerHealth.playerHealth>0*/)
+        {
+            //droneAlarm.alarm = true;
+            
+        }
+    }
+}
---
+++ Assets/Scripts/IA/frustum_trigger.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class frustum_trigger : MonoBehaviour {
+
+
+    Ray ray;
+    
+    RaycastHit rayHit;
+
+    GameObject enemy;
+    GameObject player;
+    GameObject olhos;
+    EnemyPatrolAndFollow enemyPatrol;
+    bool alarm;
+	// Use this for initialization
+	void Start () 
+    {
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        player = GameObject.FindGameObjectWithTag("Player");
+        //alarm = GameObject.FindGameObjectWithTag("Enemy").GetComponent<EnemyPatrolAndFollow>().alarm;
+        enemyPatrol = GameObject.FindGameObjectWithTag("Enemy").GetComponent<EnemyPatrolAndFollow>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        //Debug.Log("TRIGGER");
+        //Debug.Log(other.gameObject.tag);
+        //if (other.gameObject.tag == "Player")
+        //{
+        //    Debug.Log("trigger player");
+            
+
+            ray.origin = enemy.transform.position + new Vector3(0, 1.5f, 0);
+            ray.direction = (player.transform.position) - (enemy.transform.position);
+
+
+            if (Physics.Raycast(ray, out rayHit))
+            {
+                if (rayHit.transform.tag == "Player")
+                {
+                    enemyPatrol.alarm = true;
+                    Debug.Log("Inimigo consegue ver o player!");
+                }
+                else
+                {
+                    enemyPatrol.alarm = false;
+                    Debug.Log("nao é o player");
+                    Debug.Log(rayHit.transform.tag);
+                }
+
+            }
+        //}
+    }
+}
